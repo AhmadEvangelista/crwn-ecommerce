c52fe78f4a16ec4d5c97aeb578875de3ee4d93e7
AhmadEvangelista <ahmad.evangelista@gmail.com>
2020-12-27 21:07:04 +0800

    update header

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 class ShopPage extends Component {
