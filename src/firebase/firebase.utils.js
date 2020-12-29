1f969c01de6549d3eaffcd731f8dda71d1eac141
AhmadEvangelista <ahmad.evangelista@gmail.com>
2020-12-29 23:00:37 +0800

    implemented signup fuction

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/database';
+import 'firebase/firestore';
 
 var config = {
   apiKey: 'AIzaSyDKLVZoDch_XHHf2q7-Aw7eIemyX3HWauM',
@@ -11,10 +11,30 @@ var config = {
   appId: '1:42779811061:web:1217ea8917d54a6bb639bc',
   measurementId: 'G-62PGKG9WK8',
 };
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error('error creating user ', error.message);
+    }
+  }
+  return userRef;
+};
 firebase.initializeApp(config);
 export const auth = firebase.auth();
-export const database = firebase.database();
+export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
-export default database;
+export default firestore;
