feaa058c0ce461185ce2d2e3225d8a96cc0048e4
AhmadEvangelista <ahmad.evangelista@gmail.com>
2021-01-02 15:48:08 +0800

    update checkout page

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
 
 class App extends Component {
   unsubscribeFromAuth = null;
@@ -43,6 +46,9 @@ class App extends Component {
           <Route exact path='/shop'>
             <ShopPage />
           </Route>
+          <Route exact path='/checkout'>
+            <CheckoutPage />
+          </Route>
           <Route
             exact
             path='/signin'
@@ -55,7 +61,9 @@ class App extends Component {
     );
   }
 }
-const mapStateToProps = ({ user }) => ({ currentUser: user.currentUser });
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
