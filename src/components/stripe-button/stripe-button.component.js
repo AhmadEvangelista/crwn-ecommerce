854cdbf56cda32d7ba9ef947f2ddd858e28020e6
AhmadEvangelista <ahmad.evangelista@gmail.com>
2021-01-16 23:37:07 +0800

    update payment method in checkout

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51I1XXVL2eToMSvzMMojSein8Li7TmAoq3QRxf5Smzo7WCMDaaAZd14SQpyOtH5TuPUhlwCBo3PTLbcmKL31XXr2600xymo7ypS';
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN CLothing Ltd.'
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
