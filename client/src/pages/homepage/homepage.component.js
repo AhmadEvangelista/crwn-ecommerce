74d9c6dd36c66ca95ac9a05f2f546e37820ead0d
AhmadEvangelista <ahmad.evangelista@gmail.com>
2021-03-01 22:47:39 +0800

    update adding performance optimization

--- client/src/pages/homepage/homepage.component.js
+++ client/src/pages/homepage/homepage.component.js
@@ -2,12 +2,10 @@ import React from 'react';
 import DirectoryMenu from '../../components/directory-menu/directory-menu.component.js';
 import { HomePageContainer } from './homepage.styles';
 
-const HomePage = () => {
-  return (
-    <HomePageContainer>
-      <DirectoryMenu />
-    </HomePageContainer>
-  );
-};
+const HomePage = () => (
+  <HomePageContainer>
+    <DirectoryMenu />
+  </HomePageContainer>
+);
 
 export default HomePage;
